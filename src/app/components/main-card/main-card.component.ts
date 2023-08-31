a7bdd5af698049fab3f3b86d6dd8fdea61bc17ff
Victor Escandiel <joaoescandielb@gmail.com>
2023-08-31 15:48:55 -0300

    Adding content to the Component: main-card-component;

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
-  styleUrls: ['./main-card.component.css']
+  styleUrls: ['./main-card.component.css'],
 })
 export class MainCardComponent implements OnInit {
+  public mainCardContainer: string = 'main-card-container';
+  public mainCardImg: string = 'main-card-img';
+  public mainCardSpanType: string = 'main-card-span-type';
+  public mainCardSpanDate: string = 'main-card-span-date';
+  public mainCardTitle: string = 'main-card-title';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
