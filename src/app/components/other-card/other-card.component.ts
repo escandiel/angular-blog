ff15d62c5c343331b37ea1cb26043be3772b1d5a
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-01 18:50:15 -0300

    Saving changes

--- src/app/components/other-card/other-card.component.ts
+++ src/app/components/other-card/other-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-other-card',
   templateUrl: './other-card.component.html',
-  styleUrls: ['./other-card.component.css']
+  styleUrls: ['./other-card.component.css'],
 })
 export class OtherCardComponent implements OnInit {
+  @Input()
+  public otherCardImg: string = '';
+  @Input()
+  public otherCardType: string = '';
+  @Input()
+  public otherCardDate: string = '';
+  @Input()
+  public otherCardTitle: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
