68b68d4c67d780a57b8b442d5d5a0fd6faa6c1dc
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-01 18:58:21 -0300

    Mouse cursor follow in the main title

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Renderer2,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.css'],
 })
 export class TitleComponent implements OnInit {
+  @ViewChild('title', { static: true }) 'title': ElementRef;
   public titleContainer: string = 'title-container';
-  public title: string = 'title';
-  constructor() {}
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    // obtem a posição do cursor do mouse
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    // define a sensibilidade do movimento
+    const sensitivity = 10;
+
+    // cacula o deslocamento com base na posição do cursor
+    const offsetX = (mouseX - window.innerWidth / 2) / sensitivity;
+    const offsetY = (mouseY - window.innerHeight / 2) / sensitivity;
+
+    // aplica o deslocamento à div de título
+    this.renderer.setStyle(
+      this.title.nativeElement,
+      'transform',
+      `translate(${offsetX}px, ${offsetY}px)`
+    );
+  }
 }
